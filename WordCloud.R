45e1f7190aa564281c9b5bf1dcfb697e79e2cca7
Jemin Lee <leejaymin@cnu.ac.kr>
2016-08-25 10:03:21 +0900

    add pdf and results. change codes corresponding to new PDF and frequency analysis

--- WordCloud.R
+++ WordCloud.R
@@ -13,8 +13,8 @@ set.seed(8)
 
 # Reading the text in the PDF file
 
-my_pdf <- readPDF(control=list(text="-layout"))(elem=list(uri="./papers/allPapers.pdf"), language="en")
-
+#my_pdf <- readPDF(control=list(text="-layout"))(elem=list(uri="./papers/allPapers.pdf"), language="en")
+my_pdf <- readPDF(control=list(text="-layout"))(elem=list(uri="./papers/TCE.pdf"), language="en")
 
 text_raw <- my_pdf$content
 
@@ -22,6 +22,7 @@ text_raw <- text_raw[-c(1:2)] # remove title
 text_raw <- text_raw[-c(4:12)] # remove title
 text_raw <- text_raw[-c(176:201)] #remove list of references
 
+# Clean up
 text_corpus <- Corpus(VectorSource(text_raw))
 corpus_clean <- tm_map(text_corpus, stripWhitespace)
 corpus_clean <- tm_map(corpus_clean, removeNumbers)
@@ -32,9 +33,9 @@ my_stopwords <- c("e-ii","can","due","will", # additional user-defined stop word
                   "fig","figs","figure","online", # stop words related to figure captions
                   "work", "paper") # stop words related to the journal
 corpus_clean <- tm_map(corpus_clean, removeWords, my_stopwords)
-
-# punctuation
 corpus_clean <- tm_map(corpus_clean, removePunctuation)
+
+# construct word cloud
 wordcloud(corpus_clean, min.freq=25, max.words=500, random.order=FALSE, scale= c(5, 0.1), colors=brewer.pal(8,"Dark2"))
 
 
